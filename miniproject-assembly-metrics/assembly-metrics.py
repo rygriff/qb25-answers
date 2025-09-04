e9b22b4a65a468f55200b9d53db95dee3ff6f8f4
rygriff <ryleighg2001@gmail.com>
2025-09-04 14:30:00 -0400

    Determine number of contigs

--- miniproject-assembly-metrics/assembly-metrics.py
+++ miniproject-assembly-metrics/assembly-metrics.py
@@ -5,9 +5,11 @@ import fasta
 
 my_file = open(sys.argv[1]) #io.TextIOWrapper name='caenorhabditis_remanei.PRJNA248909.WBPS19.genomic.fa' mode='r' encoding='UTF-8'> this is a pointer to the file path
 contigs = fasta.FASTAReader(my_file)
-
+i=0
 for ident, sequence in contigs:
-    print( f"Name: {ident}" )
+    print(ident)
     print( f"First 20 nucleotides: {sequence[:20]}" )
+    i += 1
+print(i)
 
     
