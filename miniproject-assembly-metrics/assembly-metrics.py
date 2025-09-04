73ab766355f9a336ce5a1b0334424c48c4be9b83
rygriff <ryleighg2001@gmail.com>
2025-09-04 16:19:43 -0400

    Lance helped fix the problem - issue with for loop and indexing styles

--- miniproject-assembly-metrics/assembly-metrics.py
+++ miniproject-assembly-metrics/assembly-metrics.py
@@ -7,15 +7,33 @@ my_file = open(sys.argv[1]) #io.TextIOWrapper name='caenorhabditis_remanei.PRJNA
 contigs = fasta.FASTAReader(my_file)
 i=0
 lengths = []
+total_length = 0
 for ident, sequence in contigs:
-    # print(ident)
-    # print( f"First 20 nucleotides: {sequence[:20]}" )
+    total_length = total_length + len(sequence)
     lengths.append(len(sequence))
     i += 1
-print(i) #i = 1591
-print(f"The sum of all contigs is {sum(lengths)}") #The sum of all contigs is 118549266
-print(f"The average contig size is {sum(lengths)/len(lengths)}") #The average contig size is 74512.42363293526
+print(f"number of contigs:{i}")
+lengths.sort( reverse=True )
 
+running_sum = 0
+for contig in lengths:
+    running_sum = running_sum + contig
+    if running_sum > total_length / 2:
+        print(f"N50 = {contig}")
+        break
+    
+# counter = 0
+# for contig in range(len(sorted_lengths)):
+#     print(sorted_lengths[contig])
+#     counter += 1
+    # if counter < 10:
+    #     print(sorted_lengths[contig])
+    # elif counter > 1581:
+    #     print(sorted_lengths[contig])
+    # else:
+    #     continue
 
+# print(counter)
 
+    
     
