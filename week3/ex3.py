abd4de8cf5bb27ea628fce25516b8f39ccf7fc99
rygriff <ryleighg2001@gmail.com>
2025-10-10 09:55:26 -0400

    I'm sort of stuck at this point trying to write the data from each field into the txt file

--- week3/ex3.py
+++ week3/ex3.py
@@ -1,7 +1,8 @@
 # sample IDs (in order, corresponding to the VCF sample columns)
 sample_ids = ["A01_62", "A01_39", "A01_63", "A01_35", "A01_31",
               "A01_27", "A01_24", "A01_23", "A01_11", "A01_09"]
-
+gt_long = open("gt_long.txt", "w")
+gt_long.write("sample_id\tchrom\tpos\tgt\n")
 # open the VCF file
 for line in open("/Users/cmdb/qb25-answers/week3/BYxRM_bam/filtered.vcf"):
     if line.startswith('#'):
@@ -13,7 +14,9 @@ for line in open("/Users/cmdb/qb25-answers/week3/BYxRM_bam/filtered.vcf"):
     pos   = fields[1]
     
     # for each sample in sample_ids:
+    for sample in sample_ids:
         # get the sample's data from fields[9], fields[10], ...
+
         # genotypes are represented by the first value before ":" in that sample's data
         # if genotype is "0" then print "0"
         # if genotype is "1" then print "1"
