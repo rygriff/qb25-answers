f262d41c7c306d5c30c7a4ae68ca0fe68235a15b
rygriff <ryleighg2001@gmail.com>
2025-10-09 21:06:53 -0400

    un-commented some lines that I had quieted while testing

--- week3/ex1.sh
+++ week3/ex1.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# cd /Users/cmdb/qb25-answers/week3/
-# # tar -xzvf BYxRM_bam.tar.gz
-# 
- cd /Users/cmdb/qb25-answers/week3/BYxRM_bam
-# 
-# for file in *.bam
-# do
-#   samtools index $file
-#   samtools view -c $file 
-# done > read_counts.txt
-# 
-# for file in *.bam
-# do 
-#   echo $file
-# done > bamListFile.txt
-# 
-# # run FreeBayes to discover variants 
+cd /Users/cmdb/qb25-answers/week3/
+# tar -xzvf BYxRM_bam.tar.gz
+cd /Users/cmdb/qb25-answers/week3/BYxRM_bam
+
+for file in *.bam
+do
+   samtools index $file
+   samtools view -c $file 
+ done > read_counts.txt
+ 
+ for file in *.bam
+ do 
+   echo $file
+ done > bamListFile.txt
+ 
+# run FreeBayes to discover variants 
 freebayes -f /Users/cmdb/qb25-answers/week2/genomes/sacCer3.fa -L /Users/cmdb/qb25-answers/week3/BYxRM_bam/bamListFile.txt --genotype-qualities -p 1 > unfiltered.vcf
 
 vcffilter -f "QUAL > 20" -f "AN > 9" unfiltered.vcf > filtered.vcf
