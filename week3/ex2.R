ba439c2237a5afab3e5c6fd18b8cd15f1c49d5ad
rygriff <ryleighg2001@gmail.com>
2025-10-22 00:46:13 -0400

    Adding figure from ex.2 as pngs

--- week3/ex2.R
+++ week3/ex2.R
@@ -13,7 +13,8 @@ p1 <- ggplot(af, aes(x = AF)) +
     y = "Number of Variants"
   ) +
   theme_minimal(base_size = 14)
-print(p1)
+ggsave("/Users/cmdb/qb25-answers/week3/ex2.1.png", plot = p1)
+
 #2.1: The histogram shows a binomial-like distribution, with most variants having intermediate allele frequencies (around 0.5). This is expected because the dataset represents a population of segregants from a cross between two yeast strains. For loci that differ between the parental strains, each allele should appear in roughly 50% of the haploid offspring.
 
 dp <- read.table("/Users/cmdb/qb25-answers/week3/DP.txt", header = TRUE)
@@ -32,7 +33,7 @@ p2 <- ggplot(dp, aes(x = DP)) +
   xlim(0, 20) +  
   theme_minimal(base_size = 14)
 
-print(p2)
+ggsave("/Users/cmdb/qb25-answers/week3/ex2.2.png", plot = p2)
 
 #2.2: The histogram shows that most variants have a per-sample coverage of 3–7 reads per variant site. This is expected because the sequencing of these samples was not performed at high depth. We would anticipate most sites to have the target coverage (low depth) with a few showing higher coverage due to random variation. This distribution is called a negative binomial.
 
